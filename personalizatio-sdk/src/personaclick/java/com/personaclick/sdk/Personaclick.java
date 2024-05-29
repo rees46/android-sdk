6cb8b1e44c7ec8206b517bad08e921fe02aec6ec
Maria Anchugova <xeewisom@gmail.com>
2024-05-29 10:03:09 +0300

    refactor (stories): reverted wrong changes

--- personalizatio-sdk/src/personaclick/java/com/personaclick/sdk/Personaclick.java
+++ personalizatio-sdk/src/personaclick/java/com/personaclick/sdk/Personaclick.java
@@ -16,7 +16,7 @@ final public class Personaclick extends SDK {
 	public static final String NOTIFICATION_TYPE = "PERSONACLICK_NOTIFICATION_TYPE";
 	public static final String NOTIFICATION_ID = "PERSONACLICK_NOTIFICATION_ID";
 	protected static final String PREFERENCES_KEY = "personaclick.sdk";
-	protected static final String API_URL = BuildConfig.DEBUG ? "https://api.rees46.ru/" : "https://api.personaclick.com/";
+	protected static final String API_URL = BuildConfig.DEBUG ? "http://192.168.1.8:8080/" : "https://api.personaclick.com/";
 	/**
 	 * @param context application context
 	 * @param shop_id Shop key
