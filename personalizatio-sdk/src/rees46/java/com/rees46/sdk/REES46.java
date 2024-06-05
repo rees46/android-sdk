f79941b4b19d4a5bac46967864244e0d895db38d
Daniel Green <gdl@rees46.ru>
2024-06-05 15:36:14 +0200

    feat(push notification carousel): Change AsyncTask to Executor

--- personalizatio-sdk/src/rees46/java/com/rees46/sdk/REES46.java
+++ personalizatio-sdk/src/rees46/java/com/rees46/sdk/REES46.java
@@ -1,31 +1,30 @@
 package com.rees46.sdk;
 
-import android.annotation.SuppressLint;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 
 import com.personalizatio.BuildConfig;
-import com.personalizatio.OnMessageListener;
 import com.personalizatio.SDK;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 final public class REES46 extends SDK {
 
     public static final String TAG = "REES46";
-    public static final String NOTIFICATION_TYPE = "REES46_NOTIFICATION_TYPE";
-    public static final String NOTIFICATION_ID = "REES46_NOTIFICATION_ID";
+    public static final String NOTIFICATION_TYPE = "type";
+    public static final String NOTIFICATION_ID = "id";
     protected static final String PREFERENCES_KEY = "rees46.sdk";
     protected static final String API_URL = BuildConfig.DEBUG ? "http://dev.api.rees46.com:8000/" : "https://api.rees46.ru/";
     private static final String NOTIFICATION_CHANNEL = "notification_channel";
@@ -35,6 +34,8 @@ final public class REES46 extends SDK {
     private static final String TITLE_FIELD = "title";
     private static final String TYPE_FIELD = "type";
     private static final String ID_FIELD = "id";
+
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(4);
 
     private REES46(Context context, String shop_id, String api_host) {
         super(context, shop_id, api_host == null ? API_URL : "https://".concat(api_host).concat("/"), TAG, PREFERENCES_KEY, "android");
@@ -47,39 +48,38 @@ final public class REES46 extends SDK {
     public static void initialize(final Context context, String shop_id, String api_host) {
         if (instance == null) {
             instance = new REES46(context, shop_id, api_host);
-            REES46.setOnMessageListener(new OnMessageListener() {
-                @SuppressLint("StaticFieldLeak")
-                @Override
-                public void onMessage(Map<String, String> data) {
-                    new AsyncTask<String, Void, Bitmap>() {
-                        @Override
-                        protected Bitmap doInBackground(String... params) {
-                            try {
-                                InputStream in = new URL(params[0]).openStream();
-                                return BitmapFactory.decodeStream(in);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                            return null;
-                        }
-
-                        @Override
-                        protected void onPostExecute(Bitmap result) {
-                            super.onPostExecute(result);
-                            createNotification(context, data);
-                        }
-                    }.execute(data.get(IMAGE_FIELD));
+            REES46.setOnMessageListener(data -> {
+                String imageUrl = data.get(IMAGE_FIELD);
+                if (imageUrl != null && !imageUrl.isEmpty()) {
+                    executorService.submit(() -> {
+                        Bitmap bitmap = loadBitmap(imageUrl);
+                        createNotification(context, data, bitmap);
+                    });
+                } else {
+                    createNotification(context, data, null);
                 }
             });
         }
     }
 
-    private static void createNotification(Context context, Map<String, String> data) {
+    private static Bitmap loadBitmap(String url) {
+        if (url != null) {
+            try {
+                InputStream in = new URL(url).openStream();
+                return BitmapFactory.decodeStream(in);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    private static void createNotification(Context context, Map<String, String> data, Bitmap bitmap) {
         Intent intent = new Intent(context, context.getClass());
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
-        intent.putExtra(REES46.NOTIFICATION_TYPE, data.get(TYPE_FIELD));
-        intent.putExtra(REES46.NOTIFICATION_ID, data.get(ID_FIELD));
+        intent.putExtra(NOTIFICATION_TYPE, data.get(TYPE_FIELD));
+        intent.putExtra(NOTIFICATION_ID, data.get(ID_FIELD));
 
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
 
@@ -90,54 +90,18 @@ final public class REES46 extends SDK {
                 .setAutoCancel(true)
                 .setContentIntent(pendingIntent);
 
-        // Check if there is an image URL in the data
-        String imageUrl = data.get(IMAGE_FIELD);
-        if (imageUrl != null && !imageUrl.isEmpty()) {
-            // Load the image asynchronously
-            new AsyncTask<String, Void, Bitmap>() {
-                @Override
-                protected Bitmap doInBackground(String... params) {
-                    try {
-                        InputStream in = new URL(params[0]).openStream();
-                        return BitmapFactory.decodeStream(in);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        return null;
-                    }
-                }
-
-                @Override
-                protected void onPostExecute(Bitmap result) {
-                    super.onPostExecute(result);
-                    if (result != null) {
-                        // If the image is loaded successfully, set it as the large icon
-                        notificationBuilder.setLargeIcon(result)
-                                .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(result).bigLargeIcon(null));
-                    } else {
-                        // If the image failed to load, use BigTextStyle
-                        notificationBuilder.setStyle(new NotificationCompat.BigTextStyle().bigText(data.get(BODY_FIELD)));
-                    }
-
-                    // Show the notification
-                    NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-                    if (notificationManager != null) {
-                        notificationManager.notify(0, notificationBuilder.build());
-                    } else {
-                        Log.e(REES46.TAG, "NotificationManager not allowed");
-                    }
-                }
-            }.execute(imageUrl);
+        if (bitmap != null) {
+            notificationBuilder.setLargeIcon(bitmap)
+                    .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap).bigLargeIcon(null));
         } else {
-            // If there is no image URL, use BigTextStyle
             notificationBuilder.setStyle(new NotificationCompat.BigTextStyle().bigText(data.get(BODY_FIELD)));
+        }
 
-            // Show the notification
-            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            if (notificationManager != null) {
-                notificationManager.notify(0, notificationBuilder.build());
-            } else {
-                Log.e(REES46.TAG, "NotificationManager not allowed");
-            }
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            notificationManager.notify(0, notificationBuilder.build());
+        } else {
+            Log.e(TAG, "NotificationManager not allowed");
         }
     }
 }
--- sample/src/main/java/com/personalizatio/sample/AbstractSampleApplication.java
+++ sample/src/main/java/com/personalizatio/sample/AbstractSampleApplication.java
@@ -1,6 +1,5 @@
 package com.personalizatio.sample;
 
-import android.annotation.SuppressLint;
 import android.app.Application;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -8,11 +7,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
-import androidx.core.app.NotificationCompat;
 import android.util.Log;
 
-import com.personalizatio.OnMessageListener;
+import androidx.core.app.NotificationCompat;
+
 import com.personalizatio.SDK;
 import com.rees46.sdk.REES46;
 
@@ -20,6 +18,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public abstract class AbstractSampleApplication<T extends SDK> extends Application {
 
@@ -27,49 +27,40 @@ public abstract class AbstractSampleApplication<T extends SDK> extends Applicati
 
 	protected abstract void initialize();
 
-	private static final String NOTIFICATION_TYPE = "REES46_NOTIFICATION_TYPE";
-	private static final String NOTIFICATION_ID = "REES46_NOTIFICATION_ID";
-	private static final String NOTIFICATION_TITLE = "REES46_NOTIFICATION_TITLE";
-	private static final String NOTIFICATION_BODY = "REES46_NOTIFICATION_BODY";
-	private static final String NOTIFICATION_IMAGE = "REES46_NOTIFICATION_IMAGE";
+	private static final String NOTIFICATION_TYPE = "type";
+	private static final String NOTIFICATION_ID = "id";
+	private static final String NOTIFICATION_TITLE = "title";
+	private static final String NOTIFICATION_BODY = "body";
+	private static final String NOTIFICATION_IMAGE = "image";
 	private static final String NOTIFICATION_CHANNEL = "notification_channel";
+
+	private final ExecutorService executorService = Executors.newFixedThreadPool(4);
 
 	public void onCreate() {
 		super.onCreate();
 
-		//Demo shop
+		// Demo shop
 		initialize();
 		T.getSid(sid -> Log.d("APP", "sid: " + sid));
-		T.setOnMessageListener(new OnMessageListener() {
-			@SuppressLint("StaticFieldLeak")
-			@Override
-			public void onMessage(final Map<String, String> data) {
-				new AsyncTask<String, Void, Bitmap>() {
-
-					@Override
-					protected Bitmap doInBackground(String... params) {
-						if (params[0] != null) {
-							try {
-								InputStream in = new URL(params[0]).openStream();
-								return BitmapFactory.decodeStream(in);
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
-						}
-						return null;
-					}
-
-					@Override
-					protected void onPostExecute(Bitmap result) {
-						super.onPostExecute(result);
-						createNotification(getApplicationContext(), data);
-					}
-				}.execute(data.get("icon"));
-			}
-		});
+		T.setOnMessageListener(data -> executorService.submit(() -> {
+			Bitmap result = loadBitmap(data.get(NOTIFICATION_IMAGE));
+			createNotification(getApplicationContext(), data, result);
+		}));
 	}
 
-	private static void createNotification(Context context, Map<String, String> data) {
+	private Bitmap loadBitmap(String url) {
+		if (url != null) {
+			try {
+				InputStream in = new URL(url).openStream();
+				return BitmapFactory.decodeStream(in);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	private static void createNotification(Context context, Map<String, String> data, Bitmap result) {
 		Intent intent = new Intent(context, context.getClass());
 		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
@@ -85,53 +76,18 @@ public abstract class AbstractSampleApplication<T extends SDK> extends Applicati
 				.setAutoCancel(true)
 				.setContentIntent(pendingIntent);
 
-		// Check if there is an image URL in the data
-		String imageUrl = data.get(NOTIFICATION_IMAGE);
-		if (imageUrl != null && !imageUrl.isEmpty()) {
-			// Load the image asynchronously
-			new AsyncTask<String, Void, Bitmap>() {
-				@Override
-				protected Bitmap doInBackground(String... params) {
-					try {
-						InputStream in = new URL(params[0]).openStream();
-						return BitmapFactory.decodeStream(in);
-					} catch (IOException e) {
-						e.printStackTrace();
-						return null;
-					}
-				}
-
-				@Override
-				protected void onPostExecute(Bitmap result) {
-					super.onPostExecute(result);
-					if (result != null) {
-						// If the image is loaded successfully, set it as the large icon
-						notificationBuilder.setLargeIcon(result)
-								.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(result).bigLargeIcon(null));
-					} else {
-						// If the image failed to load, use BigTextStyle
-						notificationBuilder.setStyle(new NotificationCompat.BigTextStyle().bigText(data.get(NOTIFICATION_BODY)));
-					}
-
-					// Show the notification
-					NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-					if (notificationManager != null) {
-						notificationManager.notify(0, notificationBuilder.build());
-					} else {
-						Log.e(REES46.TAG, "NotificationManager not allowed");
-					}
-				}
-			}.execute(imageUrl);
+		if (result != null) {
+			notificationBuilder.setLargeIcon(result)
+					.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(result).bigLargeIcon(null));
 		} else {
-			// If there is no image URL, use BigTextStyle
 			notificationBuilder.setStyle(new NotificationCompat.BigTextStyle().bigText(data.get(NOTIFICATION_BODY)));
+		}
 
-			NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-			if (notificationManager != null) {
-				notificationManager.notify(0, notificationBuilder.build());
-			} else {
-				Log.e(REES46.TAG, "NotificationManager not allowed");
-			}
+		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		if (notificationManager != null) {
+			notificationManager.notify(0, notificationBuilder.build());
+		} else {
+			Log.e(REES46.TAG, "NotificationManager not allowed");
 		}
 	}
 }
