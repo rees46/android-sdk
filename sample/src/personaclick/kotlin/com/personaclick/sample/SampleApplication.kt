bf6b9a03c374927bf850e5b7f91395f15922a969
Maria Anchugova <xeewisom@gmail.com>
2024-08-15 16:07:15 +0700

    fix (sample): change shop id

--- sample/src/personaclick/kotlin/com/personaclick/sample/SampleApplication.kt
+++ sample/src/personaclick/kotlin/com/personaclick/sample/SampleApplication.kt
@@ -5,9 +5,9 @@ import com.personalization.sample.AbstractSampleApplication
 
 class SampleApplication : AbstractSampleApplication<Personaclick>(Personaclick.getInstance()) {
     override val shopId: String
-        get() = "357382bf66ac0ce2f1722677c59511"
+        get() = "cb0516af5da25b1b41490072e679bc"
     override val shopSecretKey: String
-        get() = "9902b2c111e006ee944833c95e9e6066"
+        get() = "secret_key"
 
     override fun initialize() {
         Personaclick.initialize(
