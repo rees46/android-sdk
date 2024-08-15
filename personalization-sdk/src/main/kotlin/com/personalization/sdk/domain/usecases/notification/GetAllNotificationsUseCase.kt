501fffcff23955fd3fe8d80555ecb1578f53ec84
Maria Anchugova <xeewisom@gmail.com>
2024-08-15 14:31:56 +0700

    feat (notification): add notification type and channel

--- personalization-sdk/src/main/kotlin/com/personalization/sdk/domain/usecases/notification/GetAllNotificationsUseCase.kt
+++ personalization-sdk/src/main/kotlin/com/personalization/sdk/domain/usecases/notification/GetAllNotificationsUseCase.kt
@@ -2,10 +2,13 @@ package com.personalization.sdk.domain.usecases.notification
 
 import com.google.gson.Gson
 import com.personalization.api.OnApiCallbackListener
+import com.personalization.api.params.NotificationChannels
+import com.personalization.api.params.NotificationTypes
 import com.personalization.api.responses.notifications.GetAllNotificationsResponse
 import com.personalization.sdk.domain.repositories.NetworkRepository
 import com.personalization.sdk.domain.repositories.NotificationRepository
 import org.json.JSONObject
+import java.util.EnumSet
 import javax.inject.Inject
 
 class GetAllNotificationsUseCase @Inject constructor(
@@ -19,13 +22,16 @@ class GetAllNotificationsUseCase @Inject constructor(
         loyaltyId: String? = null,
         externalId: String? = null,
         dateFrom: String,
-        type: String,
-        channel: String,
-        page: Int?,
-        limit: Int?,
+        types: NotificationTypes,
+        channels: NotificationChannels,
+        page: Int = DEFAULT_PAGE,
+        limit: Int = DEFAULT_LIMIT,
         onGetAllNotifications: (GetAllNotificationsResponse) -> Unit,
         onError: (Int, String?) -> Unit = { _: Int, _: String? -> }
     ) {
+        val type = getEnumsString(types)
+        val channel = getEnumsString(channels)
+
         val params = notificationRepository.getAllNotificationsParams(
             email = email,
             phone = phone,
@@ -56,7 +62,25 @@ class GetAllNotificationsUseCase @Inject constructor(
         )
     }
 
+    private inline fun <reified T : Enum<T>> getEnumsString(enums: EnumSet<T>): String {
+        val string = StringBuilder()
+
+        for (enum in enumValues<T>()) {
+            if(enums.contains(enum)) {
+                if(string.isNotEmpty()) {
+                    string.append(',')
+                }
+                string.append(enum.toString())
+            }
+        }
+
+        return string.toString()
+    }
+
     companion object {
         private const val GET_ALL_NOTIFICATIONS_REQUEST = "notifications"
+
+        private const val DEFAULT_PAGE = 1
+        private const val DEFAULT_LIMIT = 20
     }
 }
