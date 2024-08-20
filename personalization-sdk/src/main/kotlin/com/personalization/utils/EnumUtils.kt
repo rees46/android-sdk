6e4dfba40a1245aa0f7a3e95e2d576fd960673d4
Maria Anchugova <xeewisom@gmail.com>
2024-08-20 16:53:41 +0700

    refactor (enum): add infix function and for enum set

--- personalization-sdk/src/main/kotlin/com/personalization/utils/EnumUtils.kt
+++ personalization-sdk/src/main/kotlin/com/personalization/utils/EnumUtils.kt
@@ -7,4 +7,9 @@ object EnumUtils {
     inline fun <reified T : Enum<T>> getEnumsString(enums: EnumSet<T>): String {
         return enums.joinToString(",") { it.toString() }
     }
+
+    infix fun<T: Enum<T>> EnumSet<T>.and(other: T): EnumSet<T> {
+        this.add(other)
+        return this
+    }
 }
