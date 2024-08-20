c201d8d61badc527c13b6167e468d43ae5a694f4
Maria Anchugova <61105276+xeewii@users.noreply.github.com>
2024-08-20 14:18:07 +0700

    Update personalization-sdk/src/main/kotlin/com/personalization/sdk/data/models/params/GetAllNotificationsParams.enum.kt
    Co-authored-by: Andrew Ghostuhin <me@torinasakura.name>
    Signed-off-by: Maria Anchugova <61105276+xeewii@users.noreply.github.com>

--- personalization-sdk/src/main/kotlin/com/personalization/sdk/data/models/params/GetAllNotificationsParams.enum.kt
+++ personalization-sdk/src/main/kotlin/com/personalization/sdk/data/models/params/GetAllNotificationsParams.enum.kt
@@ -1,6 +1,18 @@
 package com.personalization.sdk.data.models.params
 
-enum class GetAllNotificationsParams(val value: String) {
+class GetAllNotificationsParams {
+    companion object {
+        const val EMAIL = "email"
+        const val PHONE = "phone"
+        const val EXTERNAL_ID = "external_id"
+        const val LOYALTY_ID = "loyalty_id"
+        const val DATE_FROM = "date_from"
+        const val TYPE = "type"
+        const val CHANNEL = "channel"
+        const val PAGE = "page"
+        const val LIMIT = "limit"
+    }
+}
     EMAIL("email"),
     PHONE("phone"),
     EXTERNAL_ID("external_id"),
