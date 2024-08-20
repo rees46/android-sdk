c4e5588427a895f98dc6f6a058bdb4c7e9bcde79
Maria Anchugova <61105276+xeewii@users.noreply.github.com>
2024-08-20 14:18:18 +0700

    Update personalization-sdk/src/main/kotlin/com/personalization/utils/EnumUtils.kt
    Co-authored-by: Andrew Ghostuhin <me@torinasakura.name>
    Signed-off-by: Maria Anchugova <61105276+xeewii@users.noreply.github.com>

--- personalization-sdk/src/main/kotlin/com/personalization/utils/EnumUtils.kt
+++ personalization-sdk/src/main/kotlin/com/personalization/utils/EnumUtils.kt
@@ -4,7 +4,9 @@ import java.util.EnumSet
 
 object EnumUtils {
 
-    inline fun <reified T : Enum<T>> getEnumsString(enums: EnumSet<T>): String {
+inline fun <reified T : Enum<T>> getEnumsString(enums: EnumSet<T>): String {
+    return enums.joinToString(",") { it.toString() }
+}
         val string = StringBuilder()
 
         for (enum in enumValues<T>()) {
