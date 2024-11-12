f871c823cfc59e7faf2d8bb6e7c6fb0386308c1f
Daniel Green <gdl@rees46.ru>
2024-11-12 11:38:57 +0200

    feat: Switch receiver to service

---
+++ personalization-sdk/src/main/kotlin/com/personalization/features/notification/data/service/NotificationUpdateService.kt
@@ -0,0 +1,65 @@
+package com.personalization.features.notification.data.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import com.personalization.features.notification.domain.model.NotificationConstants.CURRENT_IMAGE_INDEX
+import com.personalization.features.notification.domain.model.NotificationConstants.NOTIFICATION_BODY
+import com.personalization.features.notification.domain.model.NotificationConstants.NOTIFICATION_IMAGES
+import com.personalization.features.notification.domain.model.NotificationConstants.NOTIFICATION_TITLE
+import com.personalization.features.notification.domain.model.NotificationData
+import com.personalization.features.notification.presentation.helpers.NotificationHelper
+import com.personalization.features.notification.presentation.helpers.NotificationImageHelper
+import java.io.IOException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class NotificationUpdateService : Service() {
+
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.IO + serviceJob)
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val currentIndex = intent?.getIntExtra(CURRENT_IMAGE_INDEX, 0) ?: 0
+        val images = intent?.getStringExtra(NOTIFICATION_IMAGES)
+        val title = intent?.getStringExtra(NOTIFICATION_TITLE)
+        val body = intent?.getStringExtra(NOTIFICATION_BODY)
+        println("****DETEKT LOG**** : $currentIndex")
+        println("****DETEKT LOG**** : $images")
+        println("****DETEKT LOG**** : $title")
+        println("****DETEKT LOG**** : $body")
+        if (!images.isNullOrEmpty() && !title.isNullOrEmpty() && !body.isNullOrEmpty()) {
+            println("****DETEKT LOG**** : launch")
+            serviceScope.launch {
+                try {
+                    val loadedImages = NotificationImageHelper.loadBitmaps(urls = images)
+
+                    NotificationHelper.createNotification(
+                        context = this@NotificationUpdateService,
+                        notificationId = (title + body).hashCode(),
+                        data = NotificationData(title = title, body = body, images = images),
+                        images = loadedImages,
+                        currentImageIndex = currentIndex
+                    )
+                } catch (ioException: IOException) {
+                    ioException.printStackTrace()
+                } finally {
+                    stopSelf(startId)
+                }
+            }
+        } else {
+            stopSelf(startId)
+        }
+
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceJob.cancel()
+    }
+}
--- personalization-sdk/src/main/kotlin/com/personalization/features/notification/presentation/helpers/NotificationNavigationHelper.kt
+++ personalization-sdk/src/main/kotlin/com/personalization/features/notification/presentation/helpers/NotificationNavigationHelper.kt
@@ -3,7 +3,7 @@ package com.personalization.features.notification.presentation.helpers
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import com.personalization.features.notification.data.broadcast.NotificationBroadcastReceiver
+import com.personalization.features.notification.data.service.NotificationUpdateService
 import com.personalization.features.notification.domain.model.NotificationConstants.CURRENT_IMAGE_INDEX
 import com.personalization.features.notification.domain.model.NotificationConstants.NOTIFICATION_BODY
 import com.personalization.features.notification.domain.model.NotificationConstants.NOTIFICATION_IMAGES
@@ -22,14 +22,21 @@ object NotificationNavigationHelper {
         newIndex: Int,
         action: String
     ): PendingIntent {
-        val intent = Intent(context, NotificationBroadcastReceiver::class.java).apply {
+        val intent = Intent(context, NotificationUpdateService::class.java).apply {
             this.action = action
             putExtra(CURRENT_IMAGE_INDEX, newIndex)
             putExtra(NOTIFICATION_TITLE, data.title)
             putExtra(NOTIFICATION_BODY, data.body)
             putExtra(NOTIFICATION_IMAGES, data.images)
         }
-        return PendingIntent.getBroadcast(
+
+        println("****DETEKT LOG**** createNavigationPendingIntent: $newIndex")
+        println("****DETEKT LOG**** createNavigationPendingIntent: $action")
+        println("****DETEKT LOG**** createNavigationPendingIntent: ${data.images}")
+        println("****DETEKT LOG**** createNavigationPendingIntent: ${data.title}")
+        println("****DETEKT LOG**** createNavigationPendingIntent: ${data.body}")
+
+        return PendingIntent.getService(
             /* context = */ context,
             /* requestCode = */ generateRequestCode(action, newIndex),
             /* intent = */ intent,
