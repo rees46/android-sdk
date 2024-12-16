c23aced79fe5ba9d6cc5c45bc7ca173c16afdb48
Deniel Grin <gdl@rees46.ru>
2024-12-16 10:22:43 +0200

    feat: Update checking api domain in DomainFormattingUtils
    Co-authored-by: Andrew Ghostuhin <me@torinasakura.name>
    Signed-off-by: Deniel Grin <boris.sen4ikov1994@yandex.ru>

--- personalization-sdk/src/main/kotlin/com/personalization/utils/DomainFormattingUtils.kt
+++ personalization-sdk/src/main/kotlin/com/personalization/utils/DomainFormattingUtils.kt
@@ -1,9 +1,20 @@
-package com.personalization.utils
-
 object DomainFormattingUtils {
 
-    fun formatApiDomain(apiDomain: String): String = when {
-        apiDomain.endsWith("/") -> "https://$apiDomain"
-        else -> "https://$apiDomain/"
+    private val URL_REGEX = Regex("^(https?://)?.+\\..+")
+
+    fun formatApiDomain(apiDomain: String): String {
+        require(apiDomain.isNotBlank()) { "API domain cannot be blank" }
+
+        if (!URL_REGEX.matches(apiDomain)) {
+            throw IllegalArgumentException("Invalid domain: $apiDomain")
+        }
+
+        val formattedDomain = if (!apiDomain.startsWith("http://") && !apiDomain.startsWith("https://")) {
+            "https://$apiDomain"
+        } else {
+            apiDomain
+        }
+
+        return formattedDomain.trimEnd('/')
     }
 }
