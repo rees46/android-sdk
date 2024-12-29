f219338a63ab30bac2e52f5d40bf63f5380ef7c7
Arina Gazhina <gazhina@internet.ru>
2024-12-29 17:22:50 +0300

    feat: add method for clearing shopping cart

--- personalization-sdk/src/main/kotlin/com/personalization/features/cart/CartManagerImpl.kt
+++ personalization-sdk/src/main/kotlin/com/personalization/features/cart/CartManagerImpl.kt
@@ -4,6 +4,7 @@ import com.google.gson.Gson
 import com.personalization.Params
 import com.personalization.api.OnApiCallbackListener
 import com.personalization.api.managers.CartManager
+import com.personalization.api.params.CartParams
 import com.personalization.api.responses.cart.CartContent
 import com.personalization.api.responses.cart.CartContentResponse
 import com.personalization.sdk.domain.usecases.network.SendNetworkMethodUseCase
@@ -11,6 +12,8 @@ import org.json.JSONObject
 import javax.inject.Inject
 
 private const val GET_CLIENT_SHOPPING_CART = "products/cart"
+private const val CLEAR_CLIENT_SHOPPING_CART = "products/cart/clear"
+private const val SHOP_SECRET = "shop_secret"
 
 internal class CartManagerImpl @Inject constructor(
     private val sendNetworkMethodUseCase: SendNetworkMethodUseCase,
@@ -38,5 +41,27 @@ internal class CartManagerImpl @Inject constructor(
                 }
             }
         )
+    }
+
+    override fun clearClientShoppingCartContent(
+        shopSecret: String,
+        params: CartParams,
+        onCartCleared: (JSONObject?) -> Unit,
+        onError: (code: Int, msg: String?) -> Unit
+    ) {
+        sendNetworkMethodUseCase.getAsync(
+            method = CLEAR_CLIENT_SHOPPING_CART,
+            params = params.put(SHOP_SECRET, shopSecret).build(),
+            listener = object : OnApiCallbackListener() {
+                override fun onSuccess(response: JSONObject?) {
+                    response?.let {
+                        onCartCleared(response)
+                    }
+                }
+
+                override fun onError(code: Int, msg: String?) {
+                    onError(code, msg)
+                }
+            })
     }
 }
