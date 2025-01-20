1343257521d21aab4d40766ca2d388058615a63d
Arina Gazhina <gazhina@internet.ru>
2025-01-20 19:39:03 +0300

    refactor: delete sample module

--- sample/src/personaclick/kotlin/com/personaclick/sample/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.personaclick.sample
-
-import com.personaclick.sdk.Personaclick
-import com.personalization.sample.AbstractMainActivity
-
-class MainActivity : AbstractMainActivity<Personaclick>(Personaclick.getInstance())
